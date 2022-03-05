048b409b027a855c7c71dc4dc5b300bfa999e612
BhavinRaichura <bhavinraichura28@gmail.com>
2022-03-05 21:38:28 +0530

    change client setting

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from pymongo import MongoClient
-from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://practicedb:practicedb@cluster0.73alj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://<user>:<password>@cluster0.73alj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-db = MongoEngine()
+db = client
 db.init_app(app)
 
 from application import routes
