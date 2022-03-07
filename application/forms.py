082cd507fc46978b4b6e585fa4f9d56f91c048d4
BhavinRaichura <bhavinraichura28@gmail.com>
2022-03-07 23:47:58 +0530

    styling part is integrated with forms.py

--- application/forms.py
+++ application/forms.py
@@ -3,20 +3,24 @@ from wtforms import StringField,PasswordField,EmailField, BooleanField,SubmitFie
 from wtforms.validators import DataRequired,Email,Length,EqualTo,ValidationError
 #from application.models import User
 
-
 class LoginForm(FlaskForm):
-    email = EmailField("Email",validators=[DataRequired(),Email()])
-    password = PasswordField("Password",validators=[DataRequired(),Length(min=8,max=15)])
-    submit = SubmitField("Login")
+    style_email={'class':'input-field','placeholder':"Email"}
+    style_pass={'class':'input-field','placeholder':"Password"}
+    style_submit={'class':"submit-btn"}
+
+    email = EmailField("Email",validators=[DataRequired(),Email()], render_kw=style_email)
+    password = PasswordField("Password",validators=[DataRequired(),Length(min=8,max=15)], render_kw=style_pass)
+    submit = SubmitField("Login",render_kw=style_submit)
 
 class RegisterForm(FlaskForm):
-    name = StringField("Name",validators=[DataRequired(),Length(min=2,max=50)])
-    email = EmailField("Email",validators=[DataRequired(),Email()])
-    password= PasswordField("Password",validators=[DataRequired(),Length(min=8,max=15)])
-    password_confirm = PasswordField("Confirm Password", validators=[DataRequired(),Length(min=6,max=15), EqualTo('password')])
-    submit = SubmitField("Signup")
+    style_name={'class':'input-field','placeholder':"Name"}
+    style_email={'class':'input-field','placeholder':"Email"}
+    style_pass={'class':'input-field','placeholder':"Password"}
+    #style_con_pass={'class':'input-field','placeholder':"Confirm Password"}
+    style_submit={'class':"submit-btn"}
 
-    #def validate_email(self,email):
-    #    user = User.objects(email=email.data).first()
-    #    if user:
-    #        raise ValidationError("Email is already in use. Pick another one.")
+    username = StringField("Name",validators=[DataRequired(),Length(min=2,max=50)],render_kw=style_name)
+    email = EmailField("Email",validators=[DataRequired(),Email()],render_kw=style_email)
+    password= PasswordField("Password",validators=[DataRequired(),Length(min=8,max=15)],render_kw=style_pass)
+    #password_confirm = PasswordField("Confirm Password", validators=[DataRequired(),Length(min=6,max=15), EqualTo('password')],render_kw=style_con_pass)
+    submit = SubmitField("Signup",render_kw=style_submit)
